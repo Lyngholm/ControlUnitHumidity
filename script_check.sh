b1fcc07b734a277cded71adeb9d48d2b5ea8a566
Lyngholm <44528246+Lyngholm@users.noreply.github.com>
2018-10-28 18:09:19 +0100

    Update script_check.sh

--- script_check.sh
+++ script_check.sh
@@ -1,1 +1,1 @@
-pgrep -f XXXDHT22FilteredWorking.py || sudo nohup python /home/pi/ThingSpeak/XXXDHT22FilteredWorking.py /dev/null &
+pgrep -f DHT22Filtered.py || sudo nohup python /home/pi/UnitControlHumidity/DHT22Filtered.py /dev/null &
