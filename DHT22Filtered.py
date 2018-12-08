e6a88b3819ec7abcc96386400d6ddd12640c8aee
Lyngholm <44528246+Lyngholm@users.noreply.github.com>
2018-12-08 10:47:45 +0100

    Bug fixed - " missing

--- DHT22Filtered.py
+++ DHT22Filtered.py
@@ -230,7 +230,7 @@ def Main():
             #   EngineControl 
             if(EngineStatus == "On"):                    #Ensure relay match current states, to avoid error during reset of controller
                 GPIO.output(RELAY, ON)                  # RELAY set GPIO24 to 1/GPIO.HIGH/True
-            if(EngineStatus == "Off):
+            if(EngineStatus == "Off"):
                 GPIO.output(RELAY, OFF)                  # RELAY set GPIO24 to 1/GPIO.HIGH/Low
             if(humidity_Sensor9 > MaxHumidityBeforeStart):# and EngineStatus == "OFF"):
                 if(humidityHighCounter > 2):
